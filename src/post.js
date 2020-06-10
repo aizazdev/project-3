f70ac531d8252074f9e1306d226ed5e17640a14b
aizazdev <aizazkhan.uos@gmail.com>
2020-06-09 21:53:37 -0700

    Update post.js

--- src/post.js
+++ src/post.js
@@ -10,7 +10,7 @@ class Post extends Component {
             <div>
                 <CreatingPost postParagraph="This is an example paragraph 1 = > " counter={1}/>
                 <CreatingPost postParagraph="This is an example paragraph 2 = > " counter={11}/>
-                <CreatingPost postParagraph="This is an example paragraph 3 =  > " counter={111}/>
+                <CreatingPost postParagraph="This is an example paragraph 3 = > " counter={111}/>
             </div>
         )
     }
