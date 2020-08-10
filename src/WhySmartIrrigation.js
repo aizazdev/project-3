038a6f1febf42e00e8393400ec20cd98baa67dbb
aizazdev <aizazkhan.uos@gmail.com>
2020-08-10 11:54:13 +0500

    Update WhySmartIrrigation.js

--- src/WhySmartIrrigation.js
+++ src/WhySmartIrrigation.js
@@ -1,23 +1,14 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+
 import Grid from '@material-ui/core/Grid';
 import Icon1 from './images/icon1.png';
 import Icon2 from './images/icon2.png';
 import Icon3 from './images/icon3.png';
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-      justifyContent:"center"
-    },
-    vegetablearea: {
-        color: "white",
-        marginTop: "50px"
-    }
-  }));
+
 
 function WhySmartIrrigation() {
-    const classes = useStyles();
+
     return(
         <div className="whysmartirrigation-area">
            
@@ -28,21 +19,21 @@ function WhySmartIrrigation() {
                    </Grid>
 
                   <Grid item xs={10} sm={3}>
-                      <img src={Icon3}></img>
+                      <img src={Icon3} alt="icon for time"></img>
                     <h3>Time Saving</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
                     </p>
                   </Grid>
                   <Grid item xs={10} sm={3}>
-                    <img src={Icon1}></img>
+                    <img src={Icon1} alt="icon for effort"></img>
                     <h3>Less Effort</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
                     </p>
                   </Grid>
                   <Grid item xs={10} sm={3}>
-                    <img src={Icon2}></img>
+                    <img src={Icon2} alt="icon for revenue"></img>
                     <h3>More Revenue</h3>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.
