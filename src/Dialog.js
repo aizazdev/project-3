516cbbaeb5dd3fb696e5da7983e6af666a40c76b
aizazdev <aizazkhan.uos@gmail.com>
2020-08-10 11:44:23 +0500

    Update Dialog.js

--- src/Dialog.js
+++ src/Dialog.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import Firebase from 'firebase';
-import fire from './Config';
+import firebaseConfig from './Config';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -35,7 +35,7 @@ export default function FullDialog({type, btn}) {
     }
     getUserData();
   }, [])
-
+  console.log(firebaseConfig);
   const handleClickOpen = () => {
     setOpen(true);
   };
